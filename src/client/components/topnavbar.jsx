3f6dc49a39efb34c934a0c9b144d9ad35a3436cd
georgepan <georgepan@expressdigibooks.com>
2018-08-24 16:24:33 +0300

    dropdown is bad for mobiles

--- src/client/components/topnavbar.jsx
+++ src/client/components/topnavbar.jsx
@@ -4,7 +4,7 @@ import RouterLink from '../ui-components/routerlink.jsx'
 import DropDown from '../ui-components/dropdown.jsx'
 import routes from '../router/route.js'
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
-import { breakpointIsLessThan } from '../utils/responsiveHelpers'
+import { breakpointIsLessThan, setClass } from '../utils/responsiveHelpers'
 import { roleFilterRouterLink } from '../utils/managmentHelpers'
 import { connect } from 'react-redux'
 class TopNavBar extends React.Component {
@@ -18,12 +18,21 @@ class TopNavBar extends React.Component {
   handleDropDown () {
     console.log('props', roleFilterRouterLink(routes, this.props.role.name))
     if (breakpointIsLessThan('desktop', this.props.breakpoint.size)) {
-      return <div className='navbar-item has-dropdown'>
-        <div className='column is-12 field has-addons'>
-          <DropDown placeholder='Menu' name='name' onSelectValue={this.handleDropDownLink} icon='bars' searchable={false} content={roleFilterRouterLink(routes, this.props.role.name).map((route) => {
-              return { ...route }
-            })} />
-        </div>
+      // return <div className='navbar-item has-dropdown'>
+      //   <div className='column is-12 field has-addons'>
+      //     <DropDown placeholder='Menu' name='name' onSelectValue={this.handleDropDownLink} icon='bars' searchable={false} content={roleFilterRouterLink(routes, this.props.role.name).map((route) => {
+      //         return { ...route }
+      //       })} />
+      //   </div>
+      // </div>
+      return <div className='navbar-start'>
+        {roleFilterRouterLink(routes, this.props.role.name).map((route, key) => (
+          <RouterLink
+            key={route.key}
+            to={route.path}
+            text={route.name}
+          />
+        ))}
       </div>
     } else {
       return <div className='navbar-end'>
@@ -38,7 +47,7 @@ class TopNavBar extends React.Component {
     }
   }
   render () {
-    return (<nav className="navbar is-fixed-top">
+    return (<nav className='navbar is-fixed-top'>
       {this.handleDropDown()}
     </nav>)
   }
